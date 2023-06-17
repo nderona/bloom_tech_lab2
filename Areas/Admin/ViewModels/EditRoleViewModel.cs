809507fecb627d252b6971197f12f4ef29d18683
elmedina beqiri <elmedinabeqiri@live.com>
2023-06-17 18:56:31 +0200

    edit-admin

---
+++ Areas/Admin/ViewModels/EditRoleViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+
+namespace TechPetal_Lab.Areas.Admin.ViewModels
+{
+    public class EditRoleViewModel
+    {
+        public Guid UserId { get; set; }
+
+        public string RoleId { get; set; }
+
+        public SelectListItem Roles { get; set; }
+
+        public List<string> UserRoles { get; set; }
+    }
+}
